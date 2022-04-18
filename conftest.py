618dfadc0b1c1fd0c0a7a6d4e61faeab8dc61244
Pavel Shcherbakov <pyuscherbakov@gmail.com>
2022-04-18 12:33:19 +0300

    add put method

--- conftest.py
+++ conftest.py
@@ -14,7 +14,11 @@ class ApiClient:
         url = f"{self.base_address}{path}"
         return requests.get(url=url, params=params, headers=headers)
 
+    def put(self, path="/", params=None, data=None, json=None, headers=None):
+        url = f"{self.base_address}{path}"
+        return requests.put(url=url, params=params, data=data, json=json, headers=headers)
+
 
 @pytest.fixture
-def dog_api():
-    return ApiClient(base_address="https://dog.ceo/api/")
+def test():
+    return ApiClient(base_address="https://b2c.test.fast-system.ru")
