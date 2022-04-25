c3dfaca09d83f9ee146374d611d759d83087a836
Pavel Shcherbakov <pyuscherbakov@gmail.com>
2022-04-25 09:58:25 +0300

    add tests

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -4,15 +4,36 @@ import pytest
 
 
 # TODO: прописать проверки остальных методов
-
+@pytest.mark.parametrize("product", ["ingos_kasko", "alfastrah_kasko"])
 @pytest.mark.parametrize("franchise", ["Нет", "Безусловная 15 тыс."])
 class TestContracts:
+    @allure.feature('Контракт')
+    @allure.story('Создать контракт')
+    def test_get_calculation(self, franchise):
+        contract = Contract(franchise)
+        contract.create_contract()
+
+    @allure.feature('Контракт')
+    @allure.story('Создать расчет')
+    def test_get_calculation(self, franchise, product):
+        contract = Contract(franchise)
+        contract.create_contract()
+        contract.create_calculation(product)
+
     @allure.feature('Контракт')
     @allure.story('Получить контракт')
     def test_get_calculation(self, franchise):
         contract = Contract(franchise)
         contract.create_contract()
         contract.get_contract()
+
+    @allure.feature('Контракт')
+    @allure.story('Получить расчет')
+    def test_get_calculation(self, franchise, product):
+        contract = Contract(franchise)
+        contract.create_contract()
+        contract.create_calculation(product)
+        contract.get_calculation()
 
     @allure.feature('Контракт')
     @allure.story('Обновить контракт')
