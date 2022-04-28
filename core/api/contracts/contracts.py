9cd7d55400c55451a1e44045815cd437f20b2a5c
Pavel Shcherbakov <pyuscherbakov@gmail.com>
2022-04-28 20:07:12 +0300

    add logs

--- core/api/contracts/contracts.py
+++ core/api/contracts/contracts.py
@@ -25,14 +25,20 @@ class Contract:
         logger.info(f"Вызван метод создания контракта")
         data.body_create_contract["terms"]["kasko"]["franchise"] = self.franchise
         response = self.api.post(CONTRACTS.CREATE, json=data.body_create_contract)
+
         with allure.step("Проверить статус код ответа"):
             assert_that(response.status_code, equal_to(200))
+
         with allure.step("Проверить схему ответа"):
             validate(response.json(), data.schema_create_contract)
+
         response = response.json()
+
         with allure.step("Полученное в ответе поле ID не пустое"):
             assert_that(response["data"]["id"], is_not(None))
+
         self.contract_id = response["data"]["id"]
+
         logger.info(f"Создан контракт {self.contract_id}")
         allure.attach(self.contract_id, 'Contract id', allure.attachment_type.TEXT)
 
@@ -46,8 +52,10 @@ class Contract:
         self.product_id = product
         logger.info(f"Контракт отправлен на расчет. Contract ID: {self.contract_id}")
         response = self.api.post(CONTRACTS.CALCULATE.format(self.contract_id), json=data.body_create_calculation)
+
         with allure.step("Проверить статус код ответа"):
             assert_that(response.status_code, is_in([200, 202]))
+
         with allure.step("Проверить схему ответа"):
             validate(response.json(), data.schema_create_calculation)
 
@@ -58,26 +66,32 @@ class Contract:
         logger.info(f"Получить расчет. Попытка: {self.cnt_get_calc}. Contract ID: {self.contract_id}")
         response = self.api.get(CONTRACTS.CALCULATE.format(self.contract_id))
         status = response.json()["products"][0]["status"]
+
         with allure.step("Проверить статус код ответа"):
             assert_that(response.status_code, equal_to(200))
+
         with allure.step("Проверить статус расчета"):
             if self.cnt_get_calc < max_returns:
                 self.cnt_get_calc += 1
                 if status == "Executing" or status == "InQueue":
                     time.sleep(5)
+
                     with allure.step(f"Статус расчета {status}. Проверить расчет еще раз."):
                         logger.info(f"Полученный статус расчета: {status}. Contract ID: {self.contract_id}")
                         self.get_calculation()
                 else:
                     assert_that(status, equal_to("Success"), f"Получена ошибка: "
                                                              f"{response.json()['products'][0]['errors']}")
+
                     with allure.step("Проверить схему ответа"):
                         validate(response.json(), data.schema_get_calculation)
-                    logger.error(f"Расчет получен. Премия: {response.json()['products'][0]['premium']}. "
-                                 f"Продукт: {self.product_id}Contract ID: {self.contract_id}")
+
+                    logger.info(f"Расчет получен. Премия: {response.json()['products'][0]['premium']}. "
+                                f"Продукт: {self.product_id}. Contract ID: {self.contract_id}")
             else:
                 if self.cnt_get_calc >= max_returns:
                     logger.error(f"Выполнено слишком много попыток получить расчет. Contract ID: {self.contract_id}")
+
                 assert_that(self.cnt_get_calc, is_not(equal_to(max_returns)), "Выполнено слишком много попыток "
                                                                               "получить расчет")
 
@@ -86,17 +100,21 @@ class Contract:
     def get_contract(self):
         logger.info(f"Получить контракт. Contract ID: {self.contract_id}")
         response = self.api.get(CONTRACTS.GET_CALCULATE.format(self.contract_id))
+
         with allure.step("Проверить статус код ответа"):
             assert_that(response.status_code, equal_to(200))
+
         with allure.step("Проверить отправленный на создание контракт с полученным"):
             with allure.step("Проверить данные ТС"):
                 for item in response.json()["data"]["vehicle"]:
                     assert_that(data.body_create_contract["vehicle"][item],
                                 equal_to(response.json()["data"]["vehicle"][item]))
+
             with allure.step("Проверить условия страхования"):
                 for item in response.json()["data"]["terms"]:
                     assert_that(data.body_create_contract["terms"][item],
                                 equal_to(response.json()["data"]["terms"][item]))
+        logger.info(f"Контракт получен. Contract ID: {self.contract_id}")
 
     @allure.step("Обновить контракт")
     @logger.catch()
@@ -130,11 +148,14 @@ class Contract:
                     assert_that(data.body_with_update_contract["terms"][item],
                                 equal_to(response.json()["data"]["terms"][item]))
 
+        logger.info(f"Контракт обновлён. Contract ID: {self.contract_id}")
+
     @allure.step("Получить обновленный контракт")
     @logger.catch()
     def get_updated_contract(self):
         logger.info(f"Получить обновленный контракт. Contract ID: {self.contract_id}")
         response = self.api.get(CONTRACTS.GET_CALCULATE.format(self.contract_id))
+
         with allure.step("Проверить статус код ответа"):
             assert_that(response.status_code, equal_to(200))
 
@@ -148,3 +169,5 @@ class Contract:
                 for item in response.json()["data"]["terms"]:
                     assert_that(data.body_with_update_contract["terms"][item],
                                 equal_to(response.json()["data"]["terms"][item]))
+
+        logger.info(f"Обновленный контракт получен. Contract ID: {self.contract_id}")
